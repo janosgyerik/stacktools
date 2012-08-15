e31ae4db0071d2dafeeef9292523ed06dc14da58
Janos Gyerik <janos.gyerik@titan2x.com>
2012-08-15 07:43:29 +0200

    made default limit 0 when listing badges

--- stacktools/badges.py
+++ stacktools/badges.py
@@ -2,8 +2,14 @@ def print_badge(badge):
     print '%6d %s: %s' % (badge.award_count, badge.name, badge.description)
 
 
-def print_badges(site, limit=10):
-    for badge in sorted(site.all_nontag_badges(), reverse=True, key=lambda s: s.award_count)[:limit]:
+def print_badges(site, limit=0):
+    badges_sorted = sorted(site.all_nontag_badges(), reverse=True, key=lambda s: s.award_count)
+    if limit:
+        badges = badges_sorted[:limit]
+    else:
+        badges = badges_sorted
+
+    for badge in badges:
         print_badge(badge)
 
 
